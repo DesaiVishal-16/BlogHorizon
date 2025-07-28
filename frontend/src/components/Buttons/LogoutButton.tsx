7da59fad9e76048293809579ebb29c4ee56e03a1
VishalDesai <desaidesai62230@gmail.com>
2025-07-28 13:18:51 +0530

    fixed import in logoutButton

--- frontend/src/components/Buttons/LogoutButton.tsx
+++ frontend/src/components/Buttons/LogoutButton.tsx
@@ -2,37 +2,41 @@ import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../../store/hooks";
 import PrimaryButton from "./PrimaryButton";
 import { useLocalStorage } from "../../utils";
-import { logout, logoutUser, User } from "../../store/slices/authSlice";
+import { logout, logoutUser } from "../../store/slices/authSlice";
+import { User } from "../../store/slices/authSlice/types";
 
-interface LogoutButtonProps{
-    className?: string;
-    icon?: React.ReactNode;
+interface LogoutButtonProps {
+  className?: string;
+  icon?: React.ReactNode;
 }
 
-const LogoutButton = ({className,icon}:LogoutButtonProps) => {
+const LogoutButton = ({ className, icon }: LogoutButtonProps) => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-const dispatch = useAppDispatch();
-const navigate = useNavigate(); 
+  const [, , removeUserData] = useLocalStorage<User | null>("user", null);
+  const [, , removeUserToken] = useLocalStorage<string | null>("token", null);
 
-const [,,removeUserData] = useLocalStorage<User | null>('user',null);
- const [,,removeUserToken] = useLocalStorage<string | null>('token',null);
+  const handleLogout = () => {
+    try {
+      removeUserData();
+      removeUserToken();
+      dispatch(logoutUser());
+      dispatch(logout());
+      navigate("/auth/login");
+    } catch (err) {
+      console.error("Logout failed", err);
+    }
+  };
 
-const handleLogout = () => {
-  try {
-    removeUserData();
-    removeUserToken();
-    dispatch(logoutUser());
-    dispatch(logout());
-    navigate('/auth/login');
-  } catch (err) {
-    console.error("Logout failed", err);
-  }
-};
-
-
-    return(
-        <PrimaryButton icon={icon} className={className} name="Logout" onClick={handleLogout}/> 
-    )
+  return (
+    <PrimaryButton
+      icon={icon}
+      className={className}
+      name="Logout"
+      onClick={handleLogout}
+    />
+  );
 };
 
 export default LogoutButton;
