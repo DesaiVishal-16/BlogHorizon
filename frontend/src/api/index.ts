298a2f1041477458feacf6023aade173ec48e611
VishalDesai <desaidesai62230@gmail.com>
2025-08-07 20:21:39 +0530

    change API_URL

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,29 +1,29 @@
 import axios, { AxiosError, AxiosRequestConfig } from "axios";
 
-interface AxiosRequestConfigWithRetry extends AxiosRequestConfig{
-    _retry?: boolean;
+interface AxiosRequestConfigWithRetry extends AxiosRequestConfig {
+  _retry?: boolean;
 }
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
-    withCredentials: true,
+  baseURL: process.env.API_URL || "http://localhost:5000/api",
+  withCredentials: true,
 });
 
 api.interceptors.response.use(
-    (response) => response,
-    async (error: AxiosError)=>{
-      const originalRequest = error.config as AxiosRequestConfigWithRetry;
-      if(error.response?.status === 401 && !originalRequest._retry){
-          originalRequest._retry = true;
-          try{
-            await api.post("/auth/refresh");
-            return api(originalRequest)
-          }catch(refreshError){
-            console.error("Refresh token expired, please log in again");
-          }
+  (response) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as AxiosRequestConfigWithRetry;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        await api.post("/auth/refresh");
+        return api(originalRequest);
+      } catch (refreshError) {
+        console.error("Refresh token expired, please log in again");
       }
-      return Promise.reject(error);
     }
+    return Promise.reject(error);
+  }
 );
 
 export default api;
