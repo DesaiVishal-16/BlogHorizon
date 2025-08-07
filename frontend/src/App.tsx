e148b2478d843de58f759945d6475a64082a619c
VishalDesai <desaidesai62230@gmail.com>
2025-08-07 21:31:48 +0530

    added hydrateFallbackElement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,72 @@
-import { createBrowserRouter, redirect, RouterProvider } from 'react-router-dom';
-import { ErrorPage, LandingPage, AuthPage, MainPage, PostsPage, ProfilePage, NewPostPage, SinglePostPage  } from './pages';
-import { Provider } from 'react-redux';
-import { persistor,store } from './store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { profileLoader } from './utils';
-import { ProtectedRoute } from './routes';
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
+import {
+  ErrorPage,
+  LandingPage,
+  AuthPage,
+  MainPage,
+  PostsPage,
+  ProfilePage,
+  NewPostPage,
+  SinglePostPage,
+} from "./pages";
+import { Provider } from "react-redux";
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { profileLoader } from "./utils";
+import { ProtectedRoute } from "./routes";
 
-function App() { 
-    const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-    const router = createBrowserRouter([
-      { path: '/', element: <LandingPage/>, errorElement: <ErrorPage/>,},
-      { path: '/auth/:type', element: <AuthPage/>, errorElement: <ErrorPage/>,
-        loader: async({params}) => {
-          const validTypes = ['register','login'];
-          if(!validTypes.includes(params.type!)){
-            return redirect('/auth/register');
-          }
-          return null;
+function App() {
+  const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+  const router = createBrowserRouter([
+    { path: "/", element: <LandingPage />, errorElement: <ErrorPage /> },
+    {
+      path: "/auth/:type",
+      element: <AuthPage />,
+      errorElement: <ErrorPage />,
+      loader: async ({ params }) => {
+        const validTypes = ["register", "login"];
+        if (!validTypes.includes(params.type!)) {
+          return redirect("/auth/register");
         }
+        return null;
       },
-      { path: '/', element:<MainPage/>,errorElement:<ErrorPage/>,
-            children:[
-               {element:<ProtectedRoute/>, children:[
-               {path: 'posts', element:<PostsPage/>},
-               {path:'create-new-post', element:<NewPostPage/>},
-               {path:'profile/:username', element:<ProfilePage/>, loader:profileLoader},
-               {path:'profile/:username/:postId',element:<SinglePostPage/>}
-              ]}
-            ]
-      },
-    ])
+    },
+    {
+      path: "/",
+      element: <MainPage />,
+      errorElement: <ErrorPage />,
+      hydrateFallbackElement: <></>,
+      children: [
+        {
+          element: <ProtectedRoute />,
+          children: [
+            { path: "posts", element: <PostsPage /> },
+            { path: "create-new-post", element: <NewPostPage /> },
+            {
+              path: "profile/:username",
+              element: <ProfilePage />,
+              loader: profileLoader,
+            },
+            { path: "profile/:username/:postId", element: <SinglePostPage /> },
+          ],
+        },
+      ],
+    },
+  ]);
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}> 
+      <PersistGate loading={null} persistor={persistor}>
         <GoogleOAuthProvider clientId={CLIENT_ID}>
-          <RouterProvider router={router}/>
+          <RouterProvider router={router} />
         </GoogleOAuthProvider>
-      </PersistGate> 
+      </PersistGate>
     </Provider>
-  )
-};
+  );
+}
 
 export default App;
--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -5,7 +5,7 @@ interface AxiosRequestConfigWithRetry extends AxiosRequestConfig {
 }
 
 const api = axios.create({
-  baseURL: `${process.env.API_URL}/api` || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   withCredentials: true,
 });
 
