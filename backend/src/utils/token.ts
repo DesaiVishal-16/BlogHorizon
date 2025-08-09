4e830c769c798f53a3689e3ba9ad3be6e87df7e0
VishalDesai <desaidesai62230@gmail.com>
2025-08-09 14:22:00 +0530

    fix login issue

--- backend/src/utils/token.ts
+++ backend/src/utils/token.ts
@@ -1,33 +1,41 @@
-import { Response } from 'express';
-import jwt from 'jsonwebtoken';
+import { Response } from "express";
+import jwt from "jsonwebtoken";
 
-export const createAccessToken = (userId: string):string => { 
-    const secret= process.env.JWT_SECRET;
-    if(!secret){
-        throw new Error('JWT_SECRET environment variable is not set');
-    }
-    return jwt.sign({id: userId}, secret, {expiresIn: process.env.JWT_ACCESS_EXPIRES_IN  || '1h'} as jwt.SignOptions);
+export const createAccessToken = (userId: string): string => {
+  const secret = process.env.JWT_SECRET;
+  if (!secret) {
+    throw new Error("JWT_SECRET environment variable is not set");
+  }
+  return jwt.sign({ id: userId }, secret, {
+    expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || "1h",
+  } as jwt.SignOptions);
 };
 
-export const createRefreshToken = (userId: string):string => {
-    const secret = process.env.JWT_REFRESH_SECRET;
-    if(!secret){
-        throw new Error('JWT_REFRESH_SECRET  environment variable is not set');
-    }
-    return jwt.sign({id: userId}, secret, {expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d'} as jwt.SignOptions);
+export const createRefreshToken = (userId: string): string => {
+  const secret = process.env.JWT_REFRESH_SECRET;
+  if (!secret) {
+    throw new Error("JWT_REFRESH_SECRET  environment variable is not set");
+  }
+  return jwt.sign({ id: userId }, secret, {
+    expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || "7d",
+  } as jwt.SignOptions);
 };
 
-export const sendAuthCookies = (res: Response, accessToken: string, refreshToken: string) => {
-    res.cookie('accessToken',accessToken,{
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-        maxAge: 60 * 60 * 1000,
-    });
-    res.cookie('refreshToken', refreshToken,{
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite:'strict',
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+export const sendAuthCookies = (
+  res: Response,
+  accessToken: string,
+  refreshToken: string
+) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 60 * 60 * 1000,
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 };
